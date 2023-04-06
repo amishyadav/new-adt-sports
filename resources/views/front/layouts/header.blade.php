94f8ce69480692e0cd712b178329b221530dfba6
amishyadav <Amishyadav6@gmail.com>
2023-04-06 09:21:56 +0530

    feat: some minor changes regarding register page.

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -102,7 +102,7 @@
                     </li>
                     @else
                         <li>
-                            <a href="{{ route('front.player.profile') }}">My Account</a>
+                            <a href="{{ route(getDashboardURL()) }}">My Account</a>
                             <ul>
                                 <li>
                                     <a class="" href="{{ route('logout') }}"
